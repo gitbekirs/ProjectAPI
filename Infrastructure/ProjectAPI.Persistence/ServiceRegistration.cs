f2c6347abca508f0cbaabfb213ad75f071cc70a7
gitbekirs <bekirsengonul66@gmail.com>
2024-02-04 20:40:40 +0300

    Add project files.

---
+++ Infrastructure/ProjectAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectAPI.Application.Abstractions;
+using ProjectAPI.Persistence.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
